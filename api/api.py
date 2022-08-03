52cbbaa83eae95ae419106fd4b91a7359b7104a9
Mark Penovich <mpenovich1@gmail.com>
2022-08-02 20:40:54 -0500

    rewrite src file

--- api/api.py
+++ api/api.py
@@ -1,9 +1,13 @@
 import time
+import requests
 from flask import Flask
 from datetime import datetime
 
 app = Flask(__name__)
+# OMDBAPI key: 68c54217
+api_url = 'http://www.omdbapi.com?apikey=68c54217&s='
 
-@app.route('/time')
-def get_current_time():
-    return {'time': datetime.fromtimestamp(time.time())}
+@app.route('/movies/<title>')
+def get_movies(title):
+    response = requests.get(api_url + title)
+    return response.json()
